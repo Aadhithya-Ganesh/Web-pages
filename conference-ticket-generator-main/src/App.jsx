7e6cc0d72c44a95d76c473db519c133ed92be75d
AadhithyaGanesh <aadhithya.ganesh@ucdconnect.ie>
2025-04-26 15:50:03 +0100

    Conference-ticket-generator-main

---
+++ conference-ticket-generator-main/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-black">
+        <div className="flex justify-center text-3xl font-bold md:text-4xl lg:text-6xl">
+          <span className="bg-black text-white">Porn</span>
+          <span className="bg-amber-600 text-black">hub</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
